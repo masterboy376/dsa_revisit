c8c5b5333c38d99365bd16d349490ccbab8c8abb
Sambhav Kaushik <92575005+masterboy376@users.noreply.github.com>
2023-10-09 15:15:32 +0530

    Create reverse_a_sentence.cpp

---
+++ stack/reverse_a_sentence.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+// reverse a stentence
+void reverseSentence(string s){
+  stack<string> st;
+  for(int i = 0;i<s.length(); i++){
+    string word = "";
+    while(s[i]!=' ' && i<s.length()){
+      word+=s[i];
+      i++;
+    }
+    st.push(word);
+  }
+  while(!st.empty()){
+    cout<<st.top()<<" ";
+    st.pop();
+  }
+  cout<<endl;
+}
+
+int main()
+{
+  string s = "Hey, how are you doing?";
+  reverseSentence(s);
+  
+  return 0;
+}
